5f9bd6bcd2aa855db936e8a1477cd13b76b94c71
Leland Krych <lkrych@cisco.com>
2019-11-29 10:36:31 -0800

    add first cpp programs to project

---
+++ project1/ask_game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int favorite_number;
+    // << insertion operator
+    // >> extraction operator
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    
+    std::cin >> favorite_number;
+
+    std::cout << "Amazing, " << favorite_number << " is my favorite number too!" << std::endl;
+
+    return 0;
+}
