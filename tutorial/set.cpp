e8a9487ff05652553960a31b5ba4ebc8ad8e79c1
Leland Krych <leland.krych@gmail.com>
2020-01-17 17:25:00 -0800

    add set notes

---
+++ tutorial/set.cpp
@@ -0,0 +1,12 @@
+set<int> s;
+s.insert(3);
+s.insert(2);
+s.insert(5);
+cout << s.count(3) << "\n"; // 1 
+cout << s.count(4) << "\n"; // 0 
+s.erase(3);
+s.insert(4);
+cout << s.count(3) << "\n"; // 0 
+cout << s.count(4) << "\n"; // 1
+s.insert(4);
+cout << s.count(4) << "\n"; // 1 all elements either exist or they dont
