45452c4b431b000ae40871cc171c946afb3888a1
Leland Krych <leland.krych@gmail.com>
2020-01-22 08:09:12 -0800

    begin competitive programming handbook

---
+++ competitive/standard_template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+freopen("input.txt", "r", stdin); 
+freopen("output.txt", "w", stdout);
+
+int main() {
+    while (cin >> x) { //reads elements from input one after another until there is no more data available
+            // code
+    }
+}
