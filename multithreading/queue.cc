d9df1c046bf6e304d8af6b2db6f8a2696ff8779d
Leland Krych <leland.krych@gmail.com>
2020-10-20 08:04:25 -0700

    get it working in single-threaded manner

--- multithreading/queue.cc
+++ multithreading/queue.cc
@@ -7,23 +7,42 @@
 std::mutex m;
 std::condition_variable cv;
 std::queue<int> q;
+bool lock_free = false;
 
 // workers consume from the queue and sum up the number they consume
-int worker_thread(int n, int *sum) {
-    const std::lock_guard<std::mutex> lock(m);
-    int r = q.front();
-    q.pop();
-    std::cout << "Thread " << n << " is adding " << r << " to it's sum." << std::endl;
-    *sum += r;
+void worker_thread(int n) {
+    int sum = 0;
+    // spin until the queue is empty
+    while (q.size() > 0 || !lock_free) {
+        //critical section    
+        std::unique_lock<std::mutex> lk(m);
+        cv.wait(lk, []{return lock_free;});
+
+        lock_free = false;
+        int r = q.front();
+        q.pop();
+        std::cout << "Thread " << n << " is adding " << r << " to it's sum." << std::endl;
+        sum += r;
+
+        lk.unlock();
+        lock_free = true;
+        cv.notify_one();
+
+    }
+    std::cout << "Thread " << n << " exited with sum " << sum << ".\n";
 }
 
 // producers add n random numbers to the queue
 void producer_thread(int nums) {
-    const std::lock_guard<std::mutex> lock(m);
+    std::unique_lock<std::mutex> lk(m);
     for (int i = 0; i < nums; i++) {
         int r = rand() % 100;
+        std::cout << "Producer Thread putting " << r << " on queue.\n";
         q.push(r);
     }
+    lk.unlock();
+    lock_free = true;
+    cv.notify_one();
 }
 
 
@@ -36,14 +55,14 @@ int main(int argc, char *argv[]) {
     int n_items = std::stoi(argv[2]);
 
     std::thread t[n_threads];
-    int sum[n_threads];
+    int sum = 0;
 
     //create producer
     std::thread p(producer_thread, n_items);
     
-    //create workers
+    // create workers
     for (int i = 0; i < n_threads; i++) {
-        t[i] = std::thread(worker_thread, i, sum[i]);
+        t[i] = std::thread(worker_thread, i);
     }
 
     //Join the threads with the main thread
