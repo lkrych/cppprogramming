83d57775351c95d0b114df7ece58e8ba3f977ed6
Leland Krych <leland.krych@gmail.com>
2020-10-28 10:20:21 -0700

    get threadpool working with object instead of arbitrary funciton

--- multithreading/pool.h
+++ multithreading/pool.h
@@ -6,6 +6,81 @@
 #include <thread>
 #include <queue>
 
+class Hello {
+    public:
+    int id;
+    Hello(int i) {
+        id = i;
+    }
+    
+    void sayHello() {
+        std::cout << "hello " << id << " world" << std::endl;
+    }
+};
+
+class thread_pool_hello {
+    std::atomic_bool done;
+    std::mutex m;
+    std::condition_variable cv;
+    std::queue<Hello*> work_queue;
+    std::vector<std::thread> threads;
+    
+    void worker_thread() {
+        std::thread::id t_id = std::this_thread::get_id();
+
+        while (!done) {
+            // std::cout << "WorkerThread " << t_id <<  " is about to check queue\n" << std::endl;
+            Hello* task;
+            std::unique_lock<std::mutex> lk(m);
+            cv.wait(lk, [this] {
+                return done || work_queue.size() > 0;
+            });
+            if (done && work_queue.size() == 0) {
+                return;
+            }
+            task = work_queue.front();
+            work_queue.pop();
+            lk.unlock();
+
+
+            std::cout << "Thread " << t_id <<  " is doing some work\n" << std::endl;
+            task->sayHello();
+        }
+    }
+    public:
+        int n_threads = 0;
+        thread_pool_hello(int n_threads): done(false) {
+            n_threads = n_threads;
+            try {
+                for(int i  = 0; i < n_threads; i++) {
+                    threads.push_back(
+                        std::thread(&thread_pool_hello::worker_thread, this)
+                    );
+                }
+            } catch(...) {
+                std::cout << "There was an error initializing the workqueue" << std::endl;
+                done = true;
+                throw;
+            }
+        }
+        ~thread_pool_hello() {
+            std::cout << "Cleaning up the threads!\n" << std::endl;
+            done = true;
+            cv.notify_all();
+            for (int i = 0; i < n_threads; i++) {
+                if (threads[i].joinable()) {
+                    threads[i].join();
+                }
+            }
+        }
+        void enqueue(Hello* h) {
+            std::unique_lock<std::mutex> lk(m);
+            work_queue.push(h);
+            lk.unlock();
+            cv.notify_one();
+        }
+};
+
 class thread_pool {
     std::atomic_bool done;
     std::mutex m;
