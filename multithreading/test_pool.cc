f16b4c892b832e7a36faf3f1814fb4adc908470a
Leland Krych <leland.krych@gmail.com>
2020-10-28 10:21:18 -0700

    test both function based and class-based threadpool

--- multithreading/test_pool.cc
+++ multithreading/test_pool.cc
@@ -10,13 +10,21 @@
 int main()
 {
     
-    thread_pool pool(2);
+    thread_pool_hello pool(2);
 
     for(int i = 0; i < 8; ++i) {
-        pool.enqueue([i] {
-            std::cout << "hello " << i << " world" << std::endl;
-        });
+        Hello* h = new Hello(i);
+        pool.enqueue(h);
     }
+
+    // test function-based threadpool
+    // thread_pool pool(2);
+
+    // for(int i = 0; i < 8; ++i) {
+    //     pool.enqueue([i] {
+    //         std::cout << "hello " << i << " world" << std::endl;
+    //     });
+    // }
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
     return 0;
 }
