30bd091f8089366b0922e5e6ef4d15921dcc9654
Leland Krych <leland.krych@gmail.com>
2020-10-28 09:56:26 -0700

    add condition variable and lock to enqueue

--- multithreading/pool.h
+++ multithreading/pool.h
@@ -1,4 +1,5 @@
 #include <atomic>
+#include <condition_variable>
 #include <iostream>
 #include <functional>
 #include <mutex>
@@ -8,31 +9,31 @@
 class thread_pool {
     std::atomic_bool done;
     std::mutex m;
+    std::condition_variable cv;
     std::queue<std::function<void()> > work_queue;
     std::vector<std::thread> threads;
     
     void worker_thread() {
         std::thread::id t_id = std::this_thread::get_id();
-        // std::cout << "WorkerThread " << t_id <<  " is about to check done " << done << "\n" << std::endl;
 
         while (!done) {
             // std::cout << "WorkerThread " << t_id <<  " is about to check queue\n" << std::endl;
             std::function<void()> task;
-            if (work_queue.size() > 0) {
-                std::unique_lock<std::mutex> lk(m);
-                task = work_queue.front();
-                work_queue.pop();
-                lk.unlock();
-
-
-                std::cout << "Thread " << t_id <<  " is doing some work\n" << std::endl;
-                task();
-            } else {
-                std::cout << "WorkerThread " << t_id <<  " is yielding\n" << std::endl;
-                std::this_thread::yield();
+            std::unique_lock<std::mutex> lk(m);
+            cv.wait(lk, [this] {
+                return done || work_queue.size() > 0;
+            });
+            if (done && work_queue.size() == 0) {
+                return;
             }
+            task = work_queue.front();
+            work_queue.pop();
+            lk.unlock();
+
+
+            std::cout << "Thread " << t_id <<  " is doing some work\n" << std::endl;
+            task();
         }
-        // std::cout << "WorkerThread " << t_id <<  " is done \n" << std::endl;
     }
     public:
         int n_threads = 0;
@@ -53,13 +54,18 @@ class thread_pool {
         ~thread_pool() {
             std::cout << "Cleaning up the threads!\n" << std::endl;
             done = true;
-            for(int i = 0; i < n_threads; i++) {
-
-                threads[i].join();
+            cv.notify_all();
+            for (int i = 0; i < n_threads; i++) {
+                if (threads[i].joinable()) {
+                    threads[i].join();
+                }
             }
         }
         template<typename FunctionType>
         void enqueue(FunctionType f) {
+            std::unique_lock<std::mutex> lk(m);
             work_queue.push(std::function<void()>(f));
+            lk.unlock();
+            cv.notify_one();
         }
 };
